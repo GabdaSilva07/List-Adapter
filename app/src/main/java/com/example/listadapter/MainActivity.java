3c8f4bcbd309964a15ee4f40873f5829740d3a33
Gabriel Da Silva <N0726678@my.ntu.ac.uk>
2021-01-26 16:23:18 +0000

    Second Commit

--- app/src/main/java/com/example/listadapter/MainActivity.java
+++ app/src/main/java/com/example/listadapter/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.listadapter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] images = {R.drawable.alpha, R.drawable.cupcake, R.drawable.donut, R.drawable.eclair, R.drawable.froyo, R.drawable.gingerbread, R.drawable.honeycomb };
+
+    String[] version = {"Android Alpha", "Android Cupcake", "Android Donut", "Android Eclair", "Android Froyo", "Android Gingerbread", "Android Honeycomb"};
+
+    String[] versionNumber = {"1.0", "1.1", "1.5", "1.6", "2.0", "2.2", "2.3"};
+
+    ListView lView;
+
+    ListAdapter lAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lView = (ListView) findViewById(R.id.androidList);
+
+        lAdapter = new ListAdapter(MainActivity.this, version, versionNumber, images);
+
+        lView.setAdapter(lAdapter);
+
+        lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, version[i]+" "+versionNumber[i], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
